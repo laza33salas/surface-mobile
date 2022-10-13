a4c3ea8b1d425e6da9fd93b90c1894c8893101e6
laza33salas <lazarocode@gmail.com>
2022-10-13 20:19:20 -0300

    home

--- App.js
+++ App.js
@@ -5,18 +5,17 @@ import { Provider } from 'react-redux';
 import store from './features/store'
 
 // persited
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
 
-let persistor = persistStore(store)
+
+
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <PersistGate loading={null} persistor={persistor}>
-          <Drawer />
-        </PersistGate>
+
+        <Drawer />
+
       </NavigationContainer>
     </Provider>
   );
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  ImageBackground,
+} from "react-native";
+import CustomImageCarousel from "../components/Carousel/CustomImageCarousel";
+
+
+
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+  
+  const data = [
+    {image: require('../assets/2.jpg')},
+    {image: require('../assets/1.jpg')},
+    {image: require('../assets/2.jpg')},
+    {image: require('../assets/33.jpg')},
+    {image: require('../assets/2.jpg')},
+  ]
 
-export default Home
+  return (
+  
+
+  <SafeAreaView style={styles.container}>
+   <ImageBackground source={require('../assets/homee.webp')}>
+    <View style={styles.containerTittle}>
+      <Text style={styles.tittle}>SEGUÍ TU ESTILO</Text>
+      <Text style={styles.tittle2}>En SURFACE te damos las mejores opciones para que seas vos mismo</Text>
+    </View>
+   </ImageBackground>
+    <Text>Productos destacados</Text>
+    <CustomImageCarousel data={data}/>
+    </SafeAreaView>
+    
+
+
+
+  )
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  tittle:{
+   textAlign: "center",
+   fontSize: 40,
+  },
+  tittle2:{
+    textAlign: "center",
+    fontSize: 20,
+   }
+});
